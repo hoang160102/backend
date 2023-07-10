07a43f6aa4aed80187cf27361b640f1c44f6b53e
hoang160102 <hoang160102@gmail.com>
2023-07-10 23:22:19 +0700

    router-controller

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const handlebars = require('express-handlebars')
 const app = express()
 const morgan = require('morgan')
 const port = 3000
+
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded())
 app.use(express.json())
@@ -14,22 +17,9 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+route(app)
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
 
-app.get('/search', (req, res) => {
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.send('');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const newsRouter = require('./new')
+const siteRouter = require('./site')
+function route(app) {
+    // app.get('/', (req, res) => {
+    //     res.render('home');
+    //   })
+    app.use('/news', newsRouter)
+    app.use('/', siteRouter)
+    
+    //   app.get('/news', (req, res) => {
+    //     res.render('news');
+    //   })
+      
+    //   app.get('/search', (req, res) => {
+    //     res.render('search');
+    //   })
+      
+    //   app.post('/search', (req, res) => {
+    //     console.log(req.body)
+    //     res.send('');
+    //   })
+}
+
+module.exports = route
